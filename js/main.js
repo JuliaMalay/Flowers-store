68430cee4e33b86f5e66b2bdc2b118c4bd5b792d
Julia Malay <malayjulia14@gmail.com>
2021-02-06 19:35:03 +0600

    created hero block with slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const flowersSlider = new Swiper('.flower-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
